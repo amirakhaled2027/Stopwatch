4ff723e1f7f44eef8b1a423a9909ad876cc8cfcf
amirakhaled2027 <155392660+amirakhaled2027@users.noreply.github.com>
2024-01-19 23:56:14 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+const milliseconds = document.querySelector(".milliseconds");
+const startButton = document.querySelector(".start");
+const stopButton = document.querySelector(".stop");
+const resetButton = document.querySelector(".reset");
+
+let startTime;
+let interval;
+
+function start() {
+    startTime = Date.now();
+    interval = setInterval(updateDisplay, 10);
+}
+
+function stop() {
+    clearInterval(interval);
+}
+
+function reset() {
+    clearInterval(interval);
+    minutes.textContent = "00";
+    seconds.textContent = "00";
+    milliseconds.textContent = "00";
+}
+
+function updateDisplay() {
+    const elapsedTime = Date.now() - startTime;
+    const minutesValue = Math.floor(elapsedTime / 60000);
+    const secondsValue = Math.floor((elapsedTime - (minutesValue * 60000)) / 1000);
+    const millisecondsValue = Math.floor((elapsedTime - (minutesValue * 60000) - (secondsValue * 1000)) / 10);
+
+    minutes.textContent = minutesValue < 10 ? '0' + minutesValue : minutesValue;
+    seconds.textContent = secondsValue < 10 ? '0' + secondsValue : secondsValue;
+    milliseconds.textContent = millisecondsValue < 10 ? '0' + millisecondsValue : millisecondsValue;
+}
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    startButton.addEventListener('click', start);
+    stopButton.addEventListener('click', stop);
+    resetButton.addEventListener('click', reset);
+  });
+
+
